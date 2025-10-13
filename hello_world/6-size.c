c5e419cec016502691d2fc1977b136591d6e8fa2
root <you@example.com>
2025-10-13 15:22:46 +0000

    a

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -3,12 +3,12 @@
 * main - type size
 * Return: ) Success
 */
-int main (void)
+int main(void)
 {
 printf("Size of a char: %zu bytes(s)", sizeof(char));
 printf("Size of an int: %zu bytes(s)", sizeof(int));
 printf("Size of a long int: %zu bytes(s)", sizeof(long int));
 printf("Size of a long long int: %zu bytes(s)", sizeof(long long int));
 printf("Size of a float: %zu bytes(s)", sizeof(float));
-return 0;
+return (0);
 }
