4c5896e0dcc87c5b67e07f81918947067075db8f
root <you@example.com>
2025-10-13 14:05:23 +0000

    a

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - Prints "Hello, World!" using puts
+ * Return: 0 on success
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle'\n'");
 return (0);
 }
