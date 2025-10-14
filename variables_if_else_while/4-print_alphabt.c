68cefeafa02b3407bace55c26a996c7b4d7ae7b2
root <badrsha321@gmail.com>
2025-10-14 13:31:13 +0000

    a

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+* main -n is random and printf print what is it
+* Return: 0 is sucess
+*/
+int main(void)
+{
+char alph;
+char alph2;
+char alph3;
+for (alph = 'a'; alph <= 'd'; alph++)
+{
+putchar(alph);
+}
+for (alph2 = 'f': alph2 <= 'p'; alph2++)
+{
+putchar(alph2);
+}
+for (alph3 = 'r': alph3 <= 'z'; alph3++)
+{
+pitchar(alph3)
+putchar('\n')
+return (0);
+}
