52dd1101c037af804866441b212c2eefd0f133a4
root <badrsha321@gmail.com>
2025-10-14 14:18:10 +0000

    a

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+* main -n is random and printf print what is it
+* Return: 0 is sucess
+*/
+int main(void)
+{
+int num;
+for (num = 0; num <= 9; num++)
+{
+putchar(num + "0")
+}
+return (0);
+}
