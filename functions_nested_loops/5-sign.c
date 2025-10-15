8aa10e40f87a02fea9219812a7a8ccea4db6251d
root <you@example.com>
2025-10-15 15:45:53 +0000

    a

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -5,19 +5,23 @@
  *
  * Return: 1 if c is lowercase, 0 otherwise
  */
-int print_sign(int n);
+int print_sign(int n)
 {
 if (n > 0)
-_putchar('+')
-return (1);
 {
-if else (n < 0)
-_putchar('-')
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
 return (-1);
 }
 else
 {
-_putchar('0')
+_putchar('0');
+
 return (0);
 }
 }
+
