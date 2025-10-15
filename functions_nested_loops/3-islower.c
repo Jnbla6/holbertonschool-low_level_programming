18ac8cae6eeb1422b76df73e8ccb1e56356137ed
root <you@example.com>
2025-10-15 14:52:29 +0000

    a

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
 #include "main.h"
 /**
-* print_alphabet_x10 - func print itd fun for it from
-*/
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
