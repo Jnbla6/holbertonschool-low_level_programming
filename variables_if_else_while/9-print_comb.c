2bb78e0a5ced229386e2c9627c8108cf6ac64139
root <you@example.com>
2025-10-15 07:58:50 +0000

    a

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+* main -n is random and printf print what is it
+* Return: 0 is sucess
+*/
+int main(void)
+{
+int num;
+for (num = '0'; num <= '9'; num++)
+{
+putchar(num);
+if (num != '9')
+{
+putchar(',');
+}
+else
+{
+}
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
