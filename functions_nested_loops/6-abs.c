155077d0ddfc1b80ff927a6100ee2ac58211714c
root <you@example.com>
2025-10-15 16:41:19 +0000

    a

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_sign - prints the sign of a number
+ * @n: the number to check
+ *
+ * Return: 1 if n is positive, 0 if n is zero, -1 if n is negative
+ */
+int _abs(int r)
+{
+if (r < 0)
+{
+_putchar(-r);
+}
+else
+{
+_putchar(r);
+}
+return (0);
+}
