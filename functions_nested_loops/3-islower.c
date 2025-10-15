a196ae5d16762907c90e35ed1e926b06cb3a1dd5
root <you@example.com>
2025-10-15 14:22:00 +0000

    a

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+* print_alphabet_x10 - func print itd fun for it from
+*/
+int _islower(int c)
+{
+if (c == 1)
+{
+return (1);
+}
+else
+{
+}
+return (0);
+}
