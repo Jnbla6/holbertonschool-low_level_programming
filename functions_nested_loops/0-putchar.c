888973f6dd223cbb7c385107ecbac9240c7f00bc
root <you@example.com>
2025-10-15 10:38:13 +0000

    a

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* main -n is random and printf print what is it
+* Return: 0 is sucess
+*/
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
---
+++ functions_nested_loops/main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "_pychar"
+/**
+* main -n is random and printf print what is it
+* Return: 0 is sucess
+*/
+int main(void)
+{
+putchar(
+return (0);
+}
