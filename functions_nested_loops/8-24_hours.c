be19c4fc9a92b0fc1a3f1f84fecc1659e2c8fa6b
root <you@example.com>
2025-10-16 13:41:22 +0000

    a

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,9 +9,10 @@
 void jack_bauer(void)
 {
 int minute;
-int hour = 0;
+int hour = -1;
 while (hour < 23)
 {
+hour++;
 for (minute = 0; minute < 60; minute++)
 {
 _putchar((hour/10) + '0');
@@ -20,7 +21,6 @@ _putchar(':');
 _putchar((minute/10) + '0');
 _putchar((minute%10) + '0');
 _putchar('\n');
-hour++;
 }
 }
 }
