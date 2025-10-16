a2c91f733416f5ba1ede757f24e1da71cd93f0eb
root <badrsha321@gmail.com>
2025-10-16 10:22:39 +0000

    a

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _abs - check of a number
- * @r: the number to check
+ * function print_last_digit - check of a number
+ * @n: the number to check
  *
- * Return: r if n is positive -r if n is negative
+ * Return: last correct
  */
 int print_last_digit(int n)
 {
 int last;
 last = n % 10;
-if ( last < 0)
+if (last < 0)
 last = -last;
 _putchar(last + '0');
-return last;
+return (last);
 }
