ddbfd7ca4fa23c47d96dc2a7743d2b2b35243150
root <badrsha321@gmail.com>
2025-10-16 10:59:17 +0000

    a

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_last_digit - check of a number
+ * @n: the number to check
+ *
+ * Return: last correct
+ */
+void jack_bauer(void)
+{
+int hour;
+for (hour = 0; hour <= 23; hour++)
+{
+printf("%d\n", hour);
+}
+}
