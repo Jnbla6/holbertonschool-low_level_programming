6460ceae8cd65109385949afed5a199c637c958d
root <you@example.com>
2025-10-16 14:03:46 +0000

    a

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * jack_bauer - check of a number
+ * the number to check
+ *
+ * Return: last correct
+ */
+void times_table(void)
+{
+int mult;
+mult=1;
+printf("%d", mult);
+}
