1bae266bdb38f1ae2e9f1282029e30f349107a27
root <you@example.com>
2025-10-18 17:17:46 +0000

    a

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - check of a number
+ * the number to check
+ *
+ * Return: last correct
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
