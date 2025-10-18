db29a2fb5892b2c98bde3a6640c67b39ae8fadfe
root <you@example.com>
2025-10-18 19:29:16 +0000

    a

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -14,16 +14,7 @@ if (n <= 98)
 {
 for (num = n; num <= 98; num++)
 {
-if (num < 10 && num >= 0)
-{
-_putchar(num + '0');
-}
-else if (num >= 10)
-{
-_putchar((num / 10) + '0');
-_putchar((num % 10) + '0');
-}
-else
+if (num < 0)
 {
 _putchar('-');
 if (num > -10)
@@ -32,9 +23,18 @@ _putchar(-num + '0');
 }
 else
 {
-_putchar(-(num / 10) + '0');
-_putchar(-(num % 10) + '0');
+_putchar((-num / 10) + '0');
+_putchar((-num % 10) + '0');
 }
+}
+else if (num < 10)
+{
+_putchar(num + '0');
+}
+else
+{
+_putchar((num / 10) + '0');
+_putchar((num % 10) + '0');
 }
 
 if (num != 98)
@@ -48,7 +48,20 @@ else
 {
 for (num = n; num >= 98; num--)
 {
-if (num < 10 && num >= 0)
+if (num < 0)
+{
+_putchar('-');
+if (num > -10)
+{
+_putchar(-num + '0');
+}
+else
+{
+_putchar((-num / 10) + '0');
+_putchar((-num % 10) + '0');
+}
+}
+else if (num < 10)
 {
 _putchar(num + '0');
 }
