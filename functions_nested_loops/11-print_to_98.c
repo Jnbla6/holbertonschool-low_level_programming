02e024708f071d062a1e285c8c911da8de3449d8
root <you@example.com>
2025-10-18 20:11:46 +0000

    a

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,75 +1,31 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
  * print_to_98 - prints all natural numbers from n to 98
- * @n: the starting number
- *
- * Return: void
+ * @n: starting number
  */
 void print_to_98(int n)
 {
-int i;
-
 if (n <= 98)
 {
-for (i = n; i <= 98; i++)
+while (n <= 98)
 {
-print_number(i);
-if (i != 98)
-{
-_putchar(',');
-_putchar(' ');
-}
+printf("%d", n);
+if (n != 98)
+printf(", ");
+n++;
 }
 }
 else
 {
-for (i = n; i >= 98; i--)
+while (n >= 98)
 {
-print_number(i);
-if (i != 98)
-{
-_putchar(',');
-_putchar(' ');
+printf("%d", n);
+if (n != 98)
+printf(", ");
+n--;
 }
 }
-}
-_putchar('\n');
-}
-
-/**
-* print_number - prints a single number
-* @num: the number to print
-*
-* Return: void
-*/
-void print_number(int num)
-{
-int abs_num;
-
-if (num < 0)
-{
-_putchar('-');
-abs_num = -num;
-}
-else
-{
-abs_num = num;
-}
-
-if (abs_num < 10)
-{
-_putchar(abs_num + '0');
-}
-else if (abs_num < 100)
-{
-_putchar((abs_num / 10) + '0');
-_putchar((abs_num % 10) + '0');
-}
-else
-{
-_putchar((abs_num / 100) + '0');
-_putchar(((abs_num / 10) % 10) + '0');
-_putchar((abs_num % 10) + '0');
-}
+printf("\n");
 }
