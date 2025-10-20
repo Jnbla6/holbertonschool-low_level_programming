79cf1c9070f809d4c5ed62178dc26300632a163f
root <you@example.com>
2025-10-20 10:15:06 +0000

    a

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * _isdigit - checks for uppercase character
+ * mul - checks for uppercase character
  * @c: character to check
  * Return: 1 if c is uppercase, 0 otherwise
  */
