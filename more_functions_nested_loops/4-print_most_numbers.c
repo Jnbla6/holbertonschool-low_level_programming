928742426e23d34589c0241cb97b1ab927333ffe
root <you@example.com>
2025-10-20 10:50:54 +0000

    a

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+/**
+ * print_numbers - checks for uppercase character
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+void print_most_numbers(void);
+{
+int a;
+for (a = 0; a <= 9; a++)
+if (a != 2 && a != 4)
+_putchar(a + '0');
+{
+_putchar('\n');
+}
+}
