882249a58470046dd28b7c51f7d229c88b3b61d5
root <you@example.com>
2025-10-20 10:09:26 +0000

    a

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to check
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isdigit(int c)
+{
+if (isdigit(c))
+{
+return (1);
+}
+else
+return (0);
+}
