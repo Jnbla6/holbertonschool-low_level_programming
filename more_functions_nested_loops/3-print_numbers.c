a18f74cbdae6fe2a5e8de2777aede57063480b49
root <you@example.com>
2025-10-20 10:33:28 +0000

    a

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+/**
+ * mul - checks for uppercase character
+ * @a: character to check
+ * @b: character to check
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+void print_numbers(void)
+{
+int a;
+for (a = 0; a<=9; a++)
+_putchar(a + '0');
+}
