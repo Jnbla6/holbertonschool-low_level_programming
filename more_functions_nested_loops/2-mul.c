6da06aae4be77e24a341988862d2d4f892515edb
root <you@example.com>
2025-10-20 10:14:46 +0000

    a

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * _isdigit - checks for uppercase character
+ * @c: character to check
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int mul(int a, int b);
+{
+return (a * b);
+}
