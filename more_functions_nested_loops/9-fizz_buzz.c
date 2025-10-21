f6d32d66d532a81cd5b83192539c9a4f33492071
root <you@example.com>
2025-10-21 11:52:31 +0000

    a

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+/**
+ * main - checks for uppercase character
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0)
+{
+printf("Fuzz");
+printf(" ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+printf(" ");
+}
+else if ((i % 3 == 0) && (i % 5 == 0))
+{
+printf("FuzzBuzz");
+printf(" ");
+}
+else
+{
+printf("%d", i);
+printf(" ");
+}
+}
+printf("\n");
+return (0);
+}
