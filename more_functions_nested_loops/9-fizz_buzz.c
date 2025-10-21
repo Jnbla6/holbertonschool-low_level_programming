7dd233350bfe156c7ba8f13aca1f8e8537f00cea
root <you@example.com>
2025-10-21 12:02:56 +0000

    a

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,23 +14,24 @@ for (i = 1; i <= 100; i++)
 if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz");
-printf(" ");
 }
 else if (i % 5 == 0)
 {
 printf("Buzz");
-printf(" ");
 }
 else if (i % 3 == 0)
 {
 printf("Fizz");
-printf(" ");
 }
 else
 {
 printf("%d", i);
+}
+if (i <100)
+{
 printf(" ");
 }
 }
+printf("\n");
 return (0);
 }
