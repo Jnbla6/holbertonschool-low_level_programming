9cca8ba87853475a1da61f1d933252b3d9c1218e
root <you@example.com>
2025-10-21 11:20:25 +0000

    a

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -3,14 +3,14 @@
 #include <ctype.h>
 #include <unistd.h>
 /**
- * print_diagonal - checks for uppercase character
- * @n : func for kda kda
+ * print_square - checks for uppercase character
+ * @size : func for kda kda
  * Return: 1 if c is uppercase, 0 otherwise
  */
 void print_square(int size)
 {
 int i, m;
-if (size <=0)
+if (size <= 0)
 {
 _putchar('\n');
 }
