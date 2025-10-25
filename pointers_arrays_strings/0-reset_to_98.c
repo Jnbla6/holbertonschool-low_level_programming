68258632fcd86da03d0ebe29854508a7b15b0afe
root <you@example.com>
2025-10-25 09:38:12 +0000

    a

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+/**
+ * reset_to_98 - checks for uppercase character
+ * @n : func for kda kda
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
