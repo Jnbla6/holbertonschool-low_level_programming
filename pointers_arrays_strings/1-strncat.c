fc1b3b47409e22a0658187d76225a02291578c90
root <you@example.com>
2025-10-25 20:23:20 +0000

    a

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -3,21 +3,19 @@
 #include <ctype.h>
 #include <unistd.h>
 /**
- * _strcat - checks for uppercase character
+ * *_strncat - checks for uppercase character
  * @dest : func for kda kda
  * @src : func for kda kda
+ * @n : func for kda kda
  * Return: 1 if c is uppercase, 0 otherwise
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    char *ptr = dest;
-
-    while (*ptr)
-        ptr++;
-        
-    while (n-- > 0 && *src)
-        *ptr++ = *src++;
-        
-    *ptr = '\0';
-    return (dest);
+char *ptr = dest;
+while (*ptr)
+ptr++;
+while (n-- > 0 && *src)
+*ptr++ = *src++;
+*ptr = '\0';
+return (dest);
 }
