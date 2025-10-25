c848dbc9746df69092ec73c9deaecc11ba4409c5
root <you@example.com>
2025-10-25 09:54:11 +0000

    a

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+/**
+ * swap_int - checks for uppercase character
+ * @b : func for kda kda
+ * @a : func for kda kda
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+void swap_int(int *a, int *b)
+{
+int g;
+g = *a;
+*a = *b;
+*b = g;
+}
