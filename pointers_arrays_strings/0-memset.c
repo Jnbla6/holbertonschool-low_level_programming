8e4536b9f2742c68797ca9b85d80a1cb32b33037
root <you@example.com>
2025-10-27 09:44:39 +0000

    a

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+/**
+ * _memset - checks for uppercase character
+ * @s : func for kda kda
+ * @b : func for kda kda
+ * @n : func for kda kda
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
+}
+}
