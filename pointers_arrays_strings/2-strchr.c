2ec567785ec63acb91820c14ba4f40cd09618d9b
root <you@example.com>
2025-10-27 11:28:24 +0000

    a

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+/**
+ * _strchr - checks for uppercase character
+ * @s : func for kda kda
+ * @c : func for kda kda
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; i != '\0'; i++)
+{
+if (s[i] == c)
+return (&s[i]);
+}
+return (NULL);
+}
