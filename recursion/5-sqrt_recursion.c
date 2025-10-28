2ce0caa592792083cd8903fcab82cbb5a8d1d34a
Your Name <your_email@example.com>
2025-10-28 21:13:03 +0300

    aa

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+/**
+ * _pow_recursion - checks for uppercase character
+ * @x : func for kda kda
+ * @y : func for kda kda
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _sqrt_recursion(int n)
+{
+    // Handle error cases
+    if (n < 0)
+    return (-1);
+    if (n == 0 || n == 0)
+    return (n);
+    // Call helper function with starting guess
+    sqrt_helper(int n, guess)
+}
+
+int sqrt_helper(int n, int guess)
+{
+    // Check if current guess is correct
+    for (guess = 1; guess < n; guess++)
+    {
+    if (guess * guess = n)
+    }
+    return (guess)
+    // If not, decide whether to go higher or lower
+    // Or return -1 if no natural square root exists
+}
