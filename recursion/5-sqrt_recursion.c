943753a1f1cc04daa68a5d81821ffd2f75b76673
Your Name <your_email@example.com>
2025-10-28 21:23:48 +0300

    asasa

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -11,19 +11,20 @@
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-    return (-1);
-    if (n == 0 || n == 1)
-    return (n);
-    sqrt_helper(n, 1)
+if (n < 0)
+return (-1);
+if (n == 0 || n == 1)
+return (n);
+
+return sqrt_helper(n, 1);
 }
 
 int sqrt_helper(int n, int guess)
 {
-    if (guess * guess == n)
-    return (guess);
-    else if (guess * guess > n)
-    return (-1);
-    else
-    return (sqrt_helper(n, guess + 1));
+if (guess * guess == n)
+return (guess);
+else if (guess * guess > n)
+return (-1);
+else
+return (sqrt_helper(n, guess + 1));
 }
