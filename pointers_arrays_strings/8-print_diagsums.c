57768e622ae245e2a7b986402cb44276559129bd
Your Name <your_email@example.com>
2025-10-28 12:59:37 +0300

    new task is done 8-print digasum

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+/**
+ * *_strcpy - checks for uppercase character
+ * @dest : func for kda kda
+ * @src : func for kda kda
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+void print_diagsums(int *a, int size)
+{
+int i;
+int sum1 = 0;
+int sum2 = 0;
+
+for (i = 0; i < size; i++)
+{
+sum1 += a[i * size + i];
+sum2 += a[i * size - (size - 1 - i)];
+}
+
+printf("%d, %d,\n", sum1, sum2);
+}
