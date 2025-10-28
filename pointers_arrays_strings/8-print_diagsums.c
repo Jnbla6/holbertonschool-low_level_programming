397cfeb70ab9e0a9ee6eff3f46a539ac533adb6d
Your Name <your_email@example.com>
2025-10-28 13:06:23 +0300

    betty

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -3,9 +3,9 @@
 #include <ctype.h>
 #include <unistd.h>
 /**
- * *_strcpy - checks for uppercase character
- * @dest : func for kda kda
- * @src : func for kda kda
+ * print_diagsums - checks for uppercase character
+ * @a : func for kda kda
+ * @size : func for kda kda
  * Return: 1 if c is uppercase, 0 otherwise
  */
 void print_diagsums(int *a, int size)
