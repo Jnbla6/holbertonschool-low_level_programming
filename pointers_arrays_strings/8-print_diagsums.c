37a2fda6219d3552b2187376eca1be9125ab8265
Your Name <your_email@example.com>
2025-10-28 13:05:21 +0300

    fixes problem

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -17,8 +17,8 @@ int sum2 = 0;
 for (i = 0; i < size; i++)
 {
 sum1 += a[i * size + i];
-sum2 += a[i * size - (size - 1 - i)];
+sum2 += a[i * size + (size - 1 - i)];
 }
 
-printf("%d, %d,\n", sum1, sum2);
+printf("%d, %d\n", sum1, sum2);
 }
