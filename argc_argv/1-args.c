37005a49ee03a37783a96196fc8e15b99719fa55
root <you@example.com>
2025-11-01 22:28:35 +0000

    a

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+
+/**
+ * main - Determines if a number is prime
+ * @argc: The number to check
+ * @argv: hello
+ * Return: 0 if n is prime, 0 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
