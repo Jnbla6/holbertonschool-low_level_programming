2e6924aafae7f2d252e6ecea4eafc15a6d9724f9
root <you@example.com>
2025-11-02 13:06:25 +0000

    a

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <unistd.h>
-
+#include <stdlib.h>
 /**
  * main - Determines if a number is prime
  * @argc: The number to check
@@ -13,11 +13,11 @@ int main(int argc, char *argv[])
 {
 if (argc != 3)
 {
-printf("Error");
+printf("Error\n");
 }
 else
 {
-printf("%d", *argv[1] * *argv[2]);
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 }
 return (0);
 }
