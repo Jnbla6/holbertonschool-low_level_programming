b3b1d3b5e7d531d0474dad70d6608b4d39051ed7
Your Name <your_email@example.com>
2025-11-03 20:56:38 +0300

    a

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Determines if a number is prime
+ * @size: The number to check
+ * @c: hello
+ * Return: 0 if n is prime, 0 otherwise
+ */
+
+char *_strdup(char *str)
+{
+int count = 0, i;
+char *k;
+if (str == NULL)
+return (NULL);
+for (; str[count] != '\0'; count++);
+k = malloc(count * sizeof(char));
+if (k == NULL)
+return (NULL);
+for (i = 0; i < count; i++)
+k[i] = str[i];
+return (k);
+}
