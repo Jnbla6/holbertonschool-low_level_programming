522f8b4f34cfb4c97f70f809e3cf37ffef5020c1
Your Name <your_email@example.com>
2025-11-04 20:13:13 +0300

    a

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - Determines if a number is prime
+ * @grid: The number to check
+ * @height: The number to check
+ * Return: 0 if n is prime, 0 otherwise
+ */
+
+
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; i++)
+free (grid[i]);
+free (grid);
+}
