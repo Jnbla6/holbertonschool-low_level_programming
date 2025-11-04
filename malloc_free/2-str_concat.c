0048515bee6e5ebba40c1384ecfaa2173336232c
Your Name <your_email@example.com>
2025-11-04 17:22:26 +0300

    a

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Determines if a number is prime
+ * @str: The number to check
+ * Return: 0 if n is prime, 0 otherwise
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+int count1 = 0, count2 = 0, i = 0;
+int count;
+int a = 0;
+char *k;
+if (s1 == NULL && s2 == NULL)
+return (NULL);
+for (; s1[count1] != '\0'; count1++)
+;
+for (; s2[count2] != '\0'; count2++)
+;
+count = count1 + count2;
+k = malloc(count + 1 * sizeof(char));
+if (k == NULL)
+return (NULL);
+while (s1[i] != '\0')
+{
+k[i] = s1[i];
+i++;
+}
+while (s2[a] != '\0')
+{
+k[i] = s2[a];
+i++;
+a++;
+}
+k[count] = '\0';
+return (k);
+}
