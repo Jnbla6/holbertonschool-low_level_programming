c1331ba70e13605756881bb62e2704535b348296
Your Name <your_email@example.com>
2025-11-05 19:12:41 +0300

    a

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - Determines if a number is prime
+ * @grid: The number to check
+ * @height: The number to check
+ * Return: 0 if n is prime, 0 otherwise
+ */
+
+
+void *malloc_checked(unsigned int b)
+{
+int *i;
+
+i = malloc(b);
+return(i);
+
+}
