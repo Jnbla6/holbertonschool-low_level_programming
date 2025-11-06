8d5b24321db38bd99ec4166982839673732bb1ff
Your Name <your_email@example.com>
2025-11-06 21:33:52 +0300

    a

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name to assign to the dog
+ * @age: age to assign to the dog
+ * @owner: owner to assign to the dog
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+
+if (d == NULL)
+return;
+
+if (d->name == NULL)
+d->name = "(nil)";
+
+if (d->owner == NULL)
+d->owner = "(nil)";
+
+
+printf("Name: %s\n", d->name);
+if (d->age == 0)
+printf("age: (nil)\n");
+else
+printf("Age: %f\n", d->age);
+printf("Owner: %s\n", d->owner);
+}
