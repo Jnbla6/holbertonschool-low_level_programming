bd3f7e60d69678f8bd9c043cd6dd720724108c63
Your Name <your_email@example.com>
2025-11-06 18:33:26 +0300

    a

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -4,14 +4,14 @@
 /**
  * struct dog - dog structure
  * @name: dog's name
- * @age: dog's age  
+ * @age: dog's age
  * @owner: dog's owner
  */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 #endif
