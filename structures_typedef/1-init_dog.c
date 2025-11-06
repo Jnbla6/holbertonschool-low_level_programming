f39f5c08f3c88e4416ec8aa7bab78bd0c970a814
Your Name <your_email@example.com>
2025-11-06 19:36:50 +0300

    a

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -11,6 +11,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+if (d == NULL)
+printf("Ok");
 
 struct dog;
 {
@@ -18,8 +20,5 @@ d->name = name;
 d->age = age;
 d->owner = owner;
 };
-
-if (*d == NULL)
-printf("Ok");
 
 }
