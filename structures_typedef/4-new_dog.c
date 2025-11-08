e2ff8b5d67359ce6b9c1042db978562e901591c7
Badr <badrsha321@gmail.com>
2025-11-08 20:34:06 +0300

    betty

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,45 +12,35 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 int i, j;
-char *k ,*h;
+char *k, *h;
 int o, n;
-
 dog_t *new_dog;
 new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
-return(NULL);
-
+return (NULL);
 for (i = 0; name[i] != '\0'; i++)
 ;
-
 for (j = 0; owner[j] != '\0'; j++)
 ;
-
 k = malloc(i + 1);
 if (k == NULL)
 {
 free(new_dog);
-return(NULL);
+return (NULL);
 }
-
-
 h = malloc(j + 1);
 if (h == NULL)
 {
 free(new_dog);
 free(k);
-return(NULL);
+return (NULL);
 }
-
 for (n = 0; n <= i; n++)
 k[n] = name[n];
-
 for (o = 0; o <= j; o++)
 h[o] = owner[o];
-
 new_dog->name = k;
 new_dog->age = age;
 new_dog->owner = h;
-
-return(new_dog);
+return (new_dog);
 }
