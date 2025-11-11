482a5388c7a2e6f026a1366fd10fb4f79c0065bf
Your Name <your_email@example.com>
2025-11-11 16:33:18 +0300

    a

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -2,13 +2,13 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter
- * on each element of an array
- * @array: array to iterate through
+ * int_index - searches for an integer
+ * @array: array to search
  * @size: size of the array
- * @action: pointer to the function to use
+ * @cmp: pointer to the function to use to compare values
  *
- * Return: Nothing.
+ * Return: index of the first element for which the cmp function does not
+ * return 0, -1 if no element matches or size <= 0
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
@@ -25,8 +25,7 @@ return (-1);
 
 for (i = 0; i < size; i++)
 if (cmp(array[i]) != 0)
-return(i);
+return (i);
 return (-1);
-
 
 }
