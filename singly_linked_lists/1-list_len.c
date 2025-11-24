f97c295f5d95c19ff7ff3b27a3bc870b03921353
JNBLA6 <badrsha321@gmail.com>
2025-11-24 20:07:08 +0300

    removing to solve task 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head of the list_t list
+ *
+ * Return: number of nodes in the list
+ */
+size_t list_len(const list_t *h)
+{
+size_t count = 0;
+
+if (!h)
+{
+return (count);
+}
+
+while (h)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+count++;
+h = h->next;
+}
+return (count);
+}
