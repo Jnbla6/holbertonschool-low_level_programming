400c288c72500426eb61f7c3966a2ec3c24cf14c
JNBLA6 <badrsha321@gmail.com>
2025-11-24 19:34:35 +0300

    testing

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,14 +9,14 @@ size_t print_list(const list_t *h)
 {
 size_t count = 0;
 
-while(h)
-{
 if (!h->str)
 {
 printf("[0] (nil)\n");
 return(count);
 }
 else
+{
+while(h)
 {
 printf("[%d] %s\n", h->len, h->str);
 count++;
