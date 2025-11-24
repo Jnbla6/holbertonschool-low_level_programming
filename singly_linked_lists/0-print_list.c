d1a9373a23c7b50c8e2f833ffc8dea278ccec2d2
JNBLA6 <badrsha321@gmail.com>
2025-11-24 19:45:25 +0300

    fixing

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,14 +9,18 @@ size_t print_list(const list_t *h)
 {
 size_t count = 0;
 
-if (!h->str)
+if (!h)
 {
-*h->str = 0;
 return(count);
 }
 
 while(h)
 {
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+break;
+}
 printf("[%d] %s\n", h->len, h->str);
 count++;
 h = h->next;
