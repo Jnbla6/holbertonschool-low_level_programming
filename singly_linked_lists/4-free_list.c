468015e40ee3992cd6779a8de45962ae53c1bd7c
JNBLA6 <badrsha321@gmail.com>
2025-11-25 14:10:09 +0300

    fixing free list function

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,19 +1,17 @@
 #include "lists.h"
 
 /**
- * add_node_end - adds a new node at the end of a list_t list
+ * free_list - frees a list_t list
  * @head: pointer to the head of the list
- * @str: string to be added to the new node
- *
- * Return: address of the new element, or NULL if it failed
  */
 
 void free_list(list_t *head)
 {
+list_t *tmp;
 while(head)
 {
-head->str = NULL;
-head->len = 0;
-head->next = head;
+tmp = head;
+head = head->next;
+free(tmp);
 }
 }
