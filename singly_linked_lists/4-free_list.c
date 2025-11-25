70ddaca48a44a5660bae9d339e2f30e3750f7eb3
JNBLA6 <badrsha321@gmail.com>
2025-11-25 13:28:57 +0300

    adding free list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: pointer to the head of the list
+ * @str: string to be added to the new node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
+
+void free_list(list_t *head)
+{
+free(head);
+}
