757e3ff8b33a319c93afc667f39a2357d1dcc2fb
JNBLA6 <badrsha321@gmail.com>
2025-12-01 17:05:02 +0300

    doc

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,12 +1,13 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a list_t list
- * @head: pointer to the head of the list
- * @str: string to be added to the new node
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list
+ * @head: pointer to the head of the dlistint_t list
+ * @n: integer to be added to the new node
  *
  * Return: address of the new element, or NULL if it failed
  */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 dlistint_t *m;
