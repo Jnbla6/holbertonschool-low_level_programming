0fe27bbf5408155423c477d03886d910c87f0ed4
JNBLA6 <badrsha321@gmail.com>
2025-12-01 16:35:04 +0300

    style code

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -14,13 +14,13 @@ size_t print_dlistint(const dlistint_t *h)
 {
 int i = 0;
 if (h == NULL)
-return(0);
+return (0);
 
-while(h)
+while (h)
 {
 printf("%d\n", h->n);
 h = h->next;
 i++;
 }
-return(i);
+return (i);
 }
