1dcf2690adf58c47fbd7c06b55074ae56b83dabb
JNBLA6 <badrsha321@gmail.com>
2025-12-02 17:51:53 +0300

    test

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -4,18 +4,31 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 unsigned int i = 0;
 dlistint_t *temp = *h;
+dlistint_t *realnode = *h;
+
+realnode = malloc(sizeof(dlistint_t));
+if (realnode == NULL)
+return (NULL);
+realnode->n = n;
+
+
 
 if (*h == NULL)
 return (NULL);
 
-while(i < idx)
+while(i < idx - 1)
 {
 temp = temp->next;
 i++;
+
 if (temp == NULL)
 return (NULL);
 }
-temp->prev->next->n = n;
 
-return(temp);
+realnode->next = temp->next;
+realnode->prev = temp;
+
+temp->next = realnode;
+
+return(realnode);
 }
