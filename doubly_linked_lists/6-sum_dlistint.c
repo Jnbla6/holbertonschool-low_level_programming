9511d8b57cdec300e0991fce3131a9b04962b246
JNBLA6 <badrsha321@gmail.com>
2025-12-02 16:20:10 +0300

    kernalll

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - returns the sum of all the data (n) of a dlistint_t linked list
+ * sum_dlistint - returns the sum of all the data (n)
  * @head: pointer to the head of the dlistint_t list
  *
  * Return: sum of all the data, or 0 if the list is empty
