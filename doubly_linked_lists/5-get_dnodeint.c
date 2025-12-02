c1e42d5ab78e0cb4e072ccf8053fb2ea58fe9de7
JNBLA6 <badrsha321@gmail.com>
2025-12-02 15:57:41 +0300

    kernl

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -3,7 +3,13 @@
 #include <string.h>
 #include <stdio.h>
 
-
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: pointer to the head of the dlistint_t list
+ * @index: index of the node, starting from 0
+ *
+ * Return: pointer to the node at the index, or NULL if it does not exist
+ */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
@@ -11,14 +17,14 @@ unsigned int i = 0;
 
 dlistint_t *fakenode = head;
 
-if(fakenode == NULL)
-return(NULL);
+if (fakenode == NULL)
+return (NULL);
 
-while(i < index)
+while (i < index)
 {
 fakenode = fakenode->next;
-if(fakenode == NULL)
-return(NULL);
+if (fakenode == NULL)
+return (NULL);
 i++;
 }
 
