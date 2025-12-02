4eab65b00c397c2920556c694a356ce8ccb1d842
JNBLA6 <badrsha321@gmail.com>
2025-12-02 15:49:01 +0300

    test

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -6,17 +6,19 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 unsigned int i = 0;
 
-dlistint_t *fakenode;
-fakenode = head;
+dlistint_t *fakenode = head;
 
 if(fakenode == NULL)
 return(NULL);
 
-while(i <= index)
+while(i < index)
 {
 fakenode = fakenode->next;
 i++;
 }
+
+if(fakenode == NULL)
+return(NULL);
 
 return (fakenode);
 }
