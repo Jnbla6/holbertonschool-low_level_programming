f627d91c985c56e27eccf4dba689c445bfe53c82
JNBLA6 <badrsha321@gmail.com>
2025-12-05 22:28:47 +0300

    test

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,11 +9,37 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    FILE *pf = fopen(filename, "r");
-    char buffer[255];
-    while(fgets(buffer, 255, pf) != NULL)
-    printf("%s\n", buffer);
+char *buffer;
+int fd;
+ssize_t bytesread, byteswrite;
 
-    fclose(pf);
-    return (letters);
+if (filename == NULL)
+return (0);
+
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+
+buffer = malloc(letters);
+if (buffer == NULL)
+return (0);
+
+bytesread = read(fd, buffer, letters);
+if (bytesread == -1)
+{
+free(buffer);
+return (0);
+}
+
+byteswrite = write(STDOUT_FILENO, buffer, bytesread);
+if (byteswrite == -1)
+{
+free(buffer);
+return (0);
+}
+
+free(buffer);
+close(fd);
+
+return (byteswrite);
 }
