2ded19001ab4811b60abdf49eb422a2c49e3e6c3
JNBLA6 <badrsha321@gmail.com>
2025-12-05 21:19:30 +0300

    test

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,6 +9,10 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    FILE *pf = fopen(filename, 'r');
+    FILE *pf = fopen(filename, "r");
+    char buffer[255];
+    while(fgets(buffer, 255, pf) != NULL)
+    printf("%s\n", buffer);
+
     fclose(pf);
 }
