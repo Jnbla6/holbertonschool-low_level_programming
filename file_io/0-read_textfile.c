ac2a533bba5b94a0460cb8420b8eae269ba213c6
JNBLA6 <badrsha321@gmail.com>
2025-12-05 22:30:17 +0300

    betty newline and doc

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,10 +3,13 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
  *
- * Return: Always 0.
+ * Return: the actual number of letters it could read and print, 0 on failure
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 char *buffer;
