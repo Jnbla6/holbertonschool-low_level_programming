031b4fba0b11af71e79e9697a6bf662611067caf
JNBLA6 <badrsha321@gmail.com>
2025-12-05 21:02:48 +0300

    test

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    FILE *pf = fopen(filename, 'r');
+    fclose(pf);
+}
