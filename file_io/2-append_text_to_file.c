bdb43cd463f6bcb2b7c87c2695aa17fd6bac7d00
JNBLA6 <badrsha321@gmail.com>
2025-12-06 20:59:06 +0300

    test

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int len = 0;
 if (filename == NULL)
 return (-1);
 
-fd = open(filename, O_APPEND);
+fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
 
@@ -27,7 +27,7 @@ if (text_content != NULL)
 while (text_content[len])
 len++;
 
-byteswrite = append(fd, text_content, len);
+byteswrite = write(fd, text_content, len);
 if (byteswrite == -1)
 {
 return (-1);
