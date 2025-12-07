97ea4373a0e96a8416565f0ce182b6f02cc11e16
JNBLA6 <badrsha321@gmail.com>
2025-12-07 16:35:24 +0300

    test

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,48 +7,58 @@ int main(int argc, char *argv[])
     char buffer[1024];
 
     if (argc != 3)
+    {
+    dprintf(2, "Usage: cp file_from file_to\n");
     exit(97);
-
+    }
     fdsrc = open(argv[1], O_RDONLY);
     if (fdsrc == -1)
-    {
-    dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-    exit(97);
-    }
-
-    fddst = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-    if (fddst == -1)
-    {
-    dprintf(2, "Error: Can't write to %s\n", argv[1]);
-    exit(97);
-    }
-
-    while((bytesread = read(fdsrc, buffer, 1024)) > 0)
-    {
-    bytesread = read(fdsrc, buffer, 1024);
-    if(bytesread == -1)
     {
     dprintf(2, "Error: Can't read from file %s\n", argv[1]);
     exit(98);
     }
 
-    byteswrite  = write(fddst, buffer, bytesread);
-    if(byteswrite == -1)
+    fddst = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+    if (fddst == -1)
     {
-    dprintf(2, "Error: Can't write to %s\n", argv[1]);
+    dprintf(2, "Error: Can't write to %s\n", argv[2]);
+    close(fdsrc);
     exit(99);
     }
+
+    while((bytesread = read(fdsrc, buffer, 1024)) > 0)
+    {
+
+    byteswrite  = write(fddst, buffer, bytesread);
+    if (byteswrite == -1 || byteswrite != bytesread)
+    {
+        dprintf(2, "Error: Can't write to %s\n", argv[2]);
+        close(fdsrc);
+        close(fddst);
+        exit(99);
     }
 
-    close(fdsrc);
-    if (close(fdsrc) == -1)
-    exit(100);
-
-    close(fddst);
-    if (close(fddst) == -1)
+        if (bytesread == -1)
     {
     dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-    exit(100);
+    close(fdsrc);
+    close(fddst);
+    exit(98);
     }
-    return(0);
+    
+    if (close(fdsrc) == -1)
+    {
+        dprintf(2, "Error: Can't close fd %d\n", fdsrc);
+        close(fddst);
+        exit(100);
+    }
+
+    if (close(fddst) == -1)
+    {
+        dprintf(2, "Error: Can't close fd %d\n", fddst);
+        exit(100);
+    }
+
+    return (0);
+}
 }
