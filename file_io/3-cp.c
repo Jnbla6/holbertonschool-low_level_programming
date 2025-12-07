02b1477844d7ea305a658a63434829a04f218f0b
JNBLA6 <badrsha321@gmail.com>
2025-12-07 15:03:03 +0300

    test

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,11 +13,11 @@ int main(int argc, char *argv[])
     fddst = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC);
 
     bytesread = (fdsrc, buffer, 1024);
-    if(bytesread == 0)
+    if(bytesread == -1)
     exit(97);
 
     byteswrite  = (fddst, buffer, bytesread);
-    if(byteswrite == 0)
+    if(byteswrite == -1)
     exit(97);
 
     close(fdsrc);
