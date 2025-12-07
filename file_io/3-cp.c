e769da5187fb0d4535c63dad1899f3b66dea70ec
JNBLA6 <badrsha321@gmail.com>
2025-12-07 16:11:54 +0300

    testestes

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -10,25 +10,45 @@ int main(int argc, char *argv[])
     exit(97);
 
     fdsrc = open(argv[1], O_RDONLY);
-    if (fdsrc == 0)
+    if (fdsrc == -1)
+    {
+    dprintf(2, "Error: Can't read from file %s\n", argv[1]);
     exit(97);
+    }
 
     fddst = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-    if (fddst == 0)
+    if (fddst == -1)
+    {
+    dprintf(2, "Error: Can't write to %s\n", argv[1]);
     exit(97);
+    }
 
-    while(fdsrc)
+    while(bytesread = read(fdsrc, buffer, 1024) > 0)
     {
     bytesread = read(fdsrc, buffer, 1024);
     if(bytesread == -1)
+    {
+    dprintf(2, "Error: Can't read from file %s\n", argv[1]);
     exit(98);
+    }
 
     byteswrite  = write(fddst, buffer, bytesread);
     if(byteswrite == -1)
-    exit(98);
+    {
+    dprintf(2, "Error: Can't write to %s\n", argv[1]);
+    exit(99);
     }
-    close(fdsrc);
-    close(fddst);
+    }
 
+    close(fdsrc);
+    if (close(fdsrc) == -1)
+    exit(100);
+
+    close(fddst);
+    if (close(fddst) == -1)
+    {
+    dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+    exit(100);
+    }
     return(0);
 }
