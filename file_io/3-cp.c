38a8e1e7a240066483f1b80b3207fe1d37201bc7
JNBLA6 <badrsha321@gmail.com>
2025-12-07 15:16:33 +0300

    tett

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -10,16 +10,23 @@ int main(int argc, char *argv[])
     exit(97);
 
     fdsrc = open(argv[1], O_RDONLY);
-    fddst = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+    if (fdsrc = 0)
+    exit(97);
 
+    fddst = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+    if (fddst = 0)
+    exit(97);
+
+    while(fdsrc)
+    {
     bytesread = read(fdsrc, buffer, 1024);
     if(bytesread == -1)
-    exit(97);
+    exit(98);
 
     byteswrite  = write(fddst, buffer, bytesread);
     if(byteswrite == -1)
-    exit(97);
-
+    exit(98);
+    }
     close(fdsrc);
     close(fddst);
 
