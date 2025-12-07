50cf03a96efe7e925bff7cee231e5e6743527d3e
JNBLA6 <badrsha321@gmail.com>
2025-12-07 15:17:48 +0300

    teesst

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -10,11 +10,11 @@ int main(int argc, char *argv[])
     exit(97);
 
     fdsrc = open(argv[1], O_RDONLY);
-    if (fdsrc = 0)
+    if (fdsrc == 0)
     exit(97);
 
     fddst = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-    if (fddst = 0)
+    if (fddst == 0)
     exit(97);
 
     while(fdsrc)
