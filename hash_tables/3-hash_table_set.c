f1cf67901e4c41b35ef28a8177e98f56d7624c0a
JNBLA6 <badrsha321@gmail.com>
2025-12-08 16:28:47 +0300

    test

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -4,13 +4,30 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned long int index;
-hash_node_t *new;
+hash_node_t *new, *temp;
 
-new->key = key;
-new->value = value;
+if (ht == NULL || key == NULL || value == NULL)
+return (0);
 
 index = key_index(key, ht->size);
 
+temp = ht->array[index];
+
+new = malloc(sizeof(hash_node_t));
+if (new == NULL)
+return (0);
+
+new->key = strdup(key);
+if (new->key == NULL)
+return(0);
+
+new->value = strdup(value);
+if (new->value == NULL)
+return(0);
+
+new->next = ht->array[index];
 ht->array[index] = new;
+
+return (1);
 
 }
