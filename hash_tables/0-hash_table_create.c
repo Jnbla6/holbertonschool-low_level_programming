f5f5c648c7e5cbca556eb0d8c2ecdf28d66764f9
JNBLA6 <badrsha321@gmail.com>
2025-12-08 12:39:31 +0300

    test

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,12 +9,10 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *new;
-new = malloc(size * sizeof (unsigned long int));
+new = calloc(size * sizeof(unsigned long int));
 if (new == NULL)
 return (NULL);
 
 
 return (new);
-
-
 }
