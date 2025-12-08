6afeaea1ac3fe4afb43ce421ac1741394133999e
JNBLA6 <badrsha321@gmail.com>
2025-12-08 13:06:22 +0300

    test

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,18 +1,21 @@
 #include "hash_tables.h"
 
-/**
- * hash_table_create - creates a hash table
- * @size: size of the array
- * Return: pointer to the newly created hash table
- */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *new;
-new = calloc(size * sizeof(unsigned long int));
-if (new == NULL)
+hash_table_t *newtable;
+
+newtable = malloc(sizeof(hash_table_t));
+if (newtable == NULL)
 return (NULL);
 
+newtable->size = size;
 
-return (new);
+newtable->array = malloc(newtable->size * sizeof(hash_node_t));
+if (newtable->array == NULL)
+return (NULL);
+
+return(newtable)
+
+
 }
