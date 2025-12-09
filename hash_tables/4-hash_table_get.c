0a3f43dfd0e7ed557600c64e380f931cdabcb0c7
Badr <badrsha321@gmail.com>
2025-12-09 11:13:08 +0300

    test

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -4,7 +4,25 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 int index;
 char *value;
+hash_node_t *temp;
+if(ht == NULL || key == NULL)
+return (NULL);
+
 index = key_index((unsigned char *)key , ht->size);
-value = ht->array[index]->value;
+
+temp = ht->array[index];
+
+if(temp->next != NULL)
+{
+while(temp->key != key)
+temp = temp->next;
+if (temp->key == NULL)
+return (NULL);
+}
+
+value = temp->value;
+if (value == NULL)
+return (NULL);
+
 return (value);
 }
