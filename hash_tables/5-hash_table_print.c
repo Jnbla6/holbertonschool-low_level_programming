7ca3219f9e570abdf97cba3ff0ae3e3c5e03a397
JNBLA6 <badrsha321@gmail.com>
2025-12-09 17:45:07 +0300

    tttr

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,10 +1,14 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: The hash table to print.
+ */
 
 void hash_table_print(const hash_table_t *ht)
 {
 hash_node_t *temp;
-long unsigned int i;
+unsigned long int i;
 int first = 1;
 
 if (ht == NULL)
