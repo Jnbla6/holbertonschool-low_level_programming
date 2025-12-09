d8c9168babfb1560f3c401eb244859b41440a90b
JNBLA6 <badrsha321@gmail.com>
2025-12-09 19:39:38 +0300

    343094

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -5,15 +5,21 @@ void hash_table_delete(hash_table_t *ht)
 hash_node_t *temp;
 long unsigned int i = 0;
 
+if (ht == NULL)
+return;
+
 for(i = 0; i < ht->size; i++)
 {
 temp = ht->array[i];
 while (temp != NULL)
 {
+temp = temp->next;
 free(temp->key);
 free(temp->value);
-}
 free(temp);
-temp = temp->next;
 }
+temp->next;
+}
+free(ht->array);
+free(ht);
 }
