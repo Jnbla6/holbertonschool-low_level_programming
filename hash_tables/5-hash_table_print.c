c91c3e93f90887254e2ac928daa7dce79a2e2f2a
JNBLA6 <badrsha321@gmail.com>
2025-12-09 17:43:37 +0300

    trtrt

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -5,6 +5,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 hash_node_t *temp;
 long unsigned int i;
+int first = 1;
 
 if (ht == NULL)
 printf("{}");
@@ -15,7 +16,10 @@ for (i = 0; i < ht->size ; i++)
 temp = ht->array[i];
 while (temp != NULL)
 {
-printf("'%s': '%s' ", temp->key, temp->value);
+if (!first)
+printf(", ");
+printf("'%s': '%s'", temp->key, temp->value);
+first = 0;
 temp = temp->next;
 }
 }
