c59d6f47068884fe8bb5195a8958c149d99aa399
JNBLA6 <badrsha321@gmail.com>
2025-12-10 15:28:46 +0300

    solve

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,6 +1,11 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: The hash table to delete.
+ */
+
 void hash_table_delete(hash_table_t *ht)
 {
 hash_node_t *node, *tempnode;
